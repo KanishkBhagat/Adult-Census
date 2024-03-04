b82035fffa21e7d6cc3e8a06bf3e0c54967214e8
Kanishk <kanishkrajbhagat@gmail.com>
2024-03-05 01:42:40 +0530

    adding heroku file

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 import pickle
-
-from flask import Flask,jsonify,render_template,request
+import json
+from flask import Flask,render_template,jsonify,request,url_for,app
 
 app=Flask(__name__)
 reg=pickle.load(open('RandomForestModel.pkl','rb'))
@@ -11,18 +11,26 @@ encoder=pickle.load(open('encoder.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('home.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    data = request.json['data']
-    data = pd.DataFrame(data, index=[0])
-    output = reg.predict(np.array(data.values))
     
-    # Convert the output to a JSON-serializable format
-    json_output = {'prediction': output[0].item()}  # Convert NumPy int32 to Python int
+# @app.route('/predict_api',methods=['POST'])
+# def predict_api():
+#     data=request.json['data']
+#     data=pd.DataFrame(data,index=[0])
+#     #data=encoder.fit_transform(data.values)
+#     output=reg.predict(data.values)
+#     json_data={'prediction':output[0].item()}
+#     return jsonify(json_data)
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predictdata():
+    data=[float(x) for x in request.form.values()]
+    data=[np.array(data)]
+    pred=reg.predict(data)
+    return render_template('home.html',prediction_text='prediction:{}'.format(pred))
     
-    return jsonify(json_output)
-
-
 if __name__=="__main__":
     app.run(debug=True)
+
+
+    
+    
